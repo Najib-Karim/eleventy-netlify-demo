616625137c2cefd1e0ca9bc1d8995756718d3bc5
Najibullah Karim <najib.karimi@hotmail.com>
2022-03-01 15:05:11 +0000

    added dir function

--- .eleventy.js
+++ .eleventy.js
@@ -2,3 +2,11 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy ("images");
     eleventyConfig.addPassthroughCopy("admin");
 }
+module.exports = function () {
+  return {
+    dir: {
+      input: 'src',
+      layouts: '_layouts'
+    }
+  };
+};
